07de81b37c8212e0e01549279db2da49b385c91b
harikirandegala <harikirandegala92@gmail.com>
2026-06-03 18:24:03 +0530

    Configure production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../context/authStore.js';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL + '/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
